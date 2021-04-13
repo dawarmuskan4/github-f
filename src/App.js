8324c9c1d9b3c2329bf00aee3432a812d3ab3f70
Sanchit Dawar <sanchitdawar34@gmail.com>
2021-04-13 22:06:19 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './Component/layout/Navbar';
 import './App.css';
+import User from './Component/User/User';
+import Home from './Component/pages/Home';
+import Alert from './Component/layout/Alert';
+import About from './Component/pages/About';
+import NotFound from './Component/pages/NotFound';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {  
+
+    return ( 
+      <GithubState>
+        <AlertState>
+          <Router>
+            <div className='App'>
+              <Navbar />
+                <div className='container'>
+                <Alert />
+                  <Switch>
+                    <Route exact path='/' component = {Home} />
+                    <Route  exact path = '/about' component ={About}/>
+                    <Route  exact path = '/user/:login' component = {User}/>
+                    <Route component = {NotFound} />
+                  </Switch>
+                </div>
+            </div>
+          </Router>
+        </AlertState>
+      </GithubState>
   );
 }
+
 
 export default App;
---
+++ src/Component/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Search from '../User/Search';
+import Users from '../User/Users';
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Search />
+      <Users />
+    </Fragment>
+)}
+
+export default Home
